f095160817596d7fe9f64a6f89c32198e232381f
Sijmen <me@sijman.nl>
2023-05-02 15:47:41 +0200

    Scale images before sending to the browser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use askama::Template;
+use image::{imageops::FilterType, ImageFormat};
 use percent_encoding::percent_decode_str;
 use serde::Deserialize;
 
@@ -142,17 +143,29 @@ async fn post_queue_move(mut req: tide::Request<()>) -> tide::Result {
     Ok("".into())
 }
 
+#[derive(Deserialize, Default)]
+#[serde(default)]
+struct ArtQuery {
+    path: String,
+    resolution: Option<(u32, u32)>,
+}
+
 async fn get_art(req: tide::Request<()>) -> tide::Result {
-    let query: IndexQuery = req.query()?;
+    let query: ArtQuery = req.query()?;
     let path = percent_decode_str(&query.path).decode_utf8_lossy();
+    println!("reading");
     let resp = if let Ok(art) = mpd::connect()?.albumart(&path) {
-        let mime = infer::get(&art)
-            .map(|k| k.mime_type())
-            .unwrap_or("application/octet-stream");
+        let image = image::load_from_memory(&art)?;
+        let image = image.resize(512, 512, FilterType::Nearest);
+
+        let mut writer = std::io::BufWriter::new(std::io::Cursor::new(Vec::new()));
+        image.write_to(&mut writer, ImageFormat::WebP)?;
+        
+        let inner = writer.into_inner()?.into_inner();
 
         tide::Response::builder(tide::StatusCode::Ok)
-            .body(art)
-            .content_type(mime)
+            .content_type("image/webp")
+            .body(inner)
             .header("cache-control", "max-age=3600")
     } else {
         tide::Response::builder(tide::StatusCode::NotFound)
